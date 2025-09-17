cddb2989a7242af72867d7caf6cb9048116d8d21
kavil <kavillatoro316@gmail.com>
2025-09-17 13:37:07 -0500

    testing format more or less confirmed with changes to integration_testing for resuing code, then for the test dir itself has been turned into just testing general rules that will be tested to check if the common functions beetwen all versions run proper

--- integration_test/adultLoginTest.dart
+++ integration_test/adultLoginTest.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:flutter/material.dart';
 import 'package:chore_pal/main.dart' as app;
+import 'moduleTesting/login.dart';
 
 /*
 
@@ -26,36 +26,11 @@ void main() {
 
     await Future.delayed(const Duration(seconds:2));
 
-    //find email and type in email
-
-    final emailLoginText = find.byWidgetPredicate((w){
-      return w is TextField && w.decoration?.labelText=='Email';
-    });
-
-    await tester.enterText(emailLoginText, 'john@example.com');
-    await tester.pumpAndSettle(); //<- this is in case for animations (not necessary as of now but just in case
-
-    //find password and type in password
-
-    final passwordLoginText = find.byWidgetPredicate((w){
-      return w is TextField && w.decoration?.labelText=='Password';
-    });
-
-    await tester.enterText(passwordLoginText, 'password!1234');
-    await tester.pumpAndSettle();
-
-
-    FocusManager.instance.primaryFocus?.unfocus();
-
-    // Find the Login button and tap it
-    final loginButton = find.text('Login');
-    expect(loginButton, findsOneWidget);
-    await tester.pumpAndSettle(); //<- this is necessary due to load times
+    await login(tester, email: "john@example.com", password:"password!1234");
 
     await Future.delayed(const Duration(seconds: 2));
 
-    await tester.tap(loginButton);
-    await tester.pumpAndSettle(); // wait for any resulting animations
+
 
   });
 }
