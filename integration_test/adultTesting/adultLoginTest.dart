7db58fe17bff6a3b75d71728efb6f7642e9fd2d5
kavil <kavillatoro316@gmail.com>
2025-09-17 17:43:18 -0500

    changed file structure again since I realized that since a lot of the code and interactions are split between child and adult this is probably better

--- integration_test/adultTesting/adultLoginTest.dart
+++ integration_test/adultTesting/adultLoginTest.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:chore_pal/main.dart' as app;
-import 'moduleTesting/login.dart';
+import '../moduleTesting/adultModules/adultLogin.dart';
 
 /*
 
--- test/general_test.dart
+++ test/general_test.dart
@@ -1,7 +1,7 @@
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../integration_test/adultLoginTest.dart' as login;
+import '../integration_test/adultTesting/adultLoginTest.dart' as login;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
