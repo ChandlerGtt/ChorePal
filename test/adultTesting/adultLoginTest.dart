e8a46272ced43dc933bce3e06ee94b3eb0e5ef02
chandler garrett <chandlergarrett17@gmail.com>
2025-09-21 13:13:55 -0500

    ?

--- test/adultTesting/adultLoginTest.dart
+++ test/adultTesting/adultLoginTest.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:chore_pal/main.dart' as app;
@@ -34,3 +35,4 @@ void main() {
 
   });
 }
+*/
