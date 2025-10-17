cd8da7b22283621f294b129a817d598dd05a7568
kavil <kavillatoro316@gmail.com>
2025-10-17 12:54:24 -0500

    didn't complete too much, just added some extra lines to tabtest, does need to go through all tab to make sure they all work without introducing some weird issue with the widgets

--- integration_test/adultTesting/adultTabTest.dart
+++ integration_test/adultTesting/adultTabTest.dart
@@ -1,9 +1,8 @@
-
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:chore_pal/main.dart' as app;
 import '../moduleTesting/adultModules/adultLogin.dart';
-
 /*
 
 notes for the following test:
@@ -20,14 +19,18 @@ and just land on the interface
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('Login functions for adult account and is tappable', (WidgetTester tester) async {
+  testWidgets('checking if swapping between tab is an issue or presents any underlying issue', (WidgetTester tester) async {
     // Launch the app
     app.main();
     await tester.pumpAndSettle(); // wait for all animations
 
-    await Future.delayed(const Duration(seconds:2));
+    await Future.delayed(const Duration(seconds : 2));
 
-    await login(tester, email: "john@example.com", password:"password!1234");
+    await login(tester, email:"john@example.com", password: "password!1234");
+
+    await Future.delayed(const Duration(seconds : 2));
+
+    await tester.tap(find.text('Statistics'));
 
     await Future.delayed(const Duration(seconds: 2));
 
